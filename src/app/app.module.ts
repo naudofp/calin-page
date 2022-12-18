2a71cb6a942132b9275ad9458a1064667651935e
naudofp <felipenaudof@gmail.com>
2022-12-18 00:50:34 -0300

    Created Navbar's Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NavbarHomeComponent } from './components/navbar-home/navbar-home.component';
+import { NavbarItemComponent } from './components/navbar-home/navbar-item/navbar-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarHomeComponent,
+    NavbarItemComponent
   ],
   imports: [
     BrowserModule,
