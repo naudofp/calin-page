60bd3dbb1cfa157543bff72106fe632324b14e8e
Matheus Christo <matheuschristo.09@gmail.com>
2022-12-18 18:33:51 -0300

    Created ViewBenefits Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { NavbarHomeComponent } from './components/navbar-home/navbar-home.component';
 import { NavbarItemComponent } from './components/navbar-home/navbar-item/navbar-item.component';
 import { ViewSloganComponent } from './components/home-page/view-slogan/view-slogan.component';
+import { ViewBenefitsComponent } from './components/home-page/view-benefits/view-benefits.component';
+import { BenefitsCardComponent } from './components/home-page/view-benefits/benefits-card/benefits-card.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ViewSloganComponent } from './components/home-page/view-slogan/view-slo
     HomePageComponent,
     NavbarHomeComponent,
     NavbarItemComponent,
-    ViewSloganComponent
+    ViewSloganComponent,
+    ViewBenefitsComponent,
+    BenefitsCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home-page/view-benefits/benefits-card/benefits-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-benefits-card',
+  templateUrl: './benefits-card.component.html',
+  styleUrls: ['./benefits-card.component.css']
+})
+export class BenefitsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
