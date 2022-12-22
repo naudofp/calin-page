b7b272ba5a7ad65fd09d15a17411c38b65311263
naudofp <felipenaudof@gmail.com>
2022-12-21 21:09:08 -0300

    Updated SloganView with new design

--- src/app/components/navbar-home/navbar-item/navbar-item.component.ts
+++ src/app/components/navbar-home/navbar-item/navbar-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-item',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-item.component.css']
 })
 export class NavbarItemComponent implements OnInit {
+
+  @Input()
+  text: String = "";
+
+  @Input()
+  icon: String = "";
 
   constructor() { }
 
